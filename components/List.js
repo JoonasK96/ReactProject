a7bd65a6d1c2c521d520b961afab7df3e5c90e8a
joonavke <joonas.kelo@metropolia.com>
2020-08-28 11:08:16 +0300

    http-a valmis

---
+++ components/List.js
@@ -0,0 +1,35 @@
+/* eslint-disable max-len */
+import React, {useState, useEffect} from 'react';
+import {FlatList} from 'react-native';
+import ListItem from './ListItem';
+
+
+const url = 'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+
+const List = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const loadMedia = async () => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setMediaArray(json);
+    } catch (error) {
+      console.error('loadMedia error', error);
+    }
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+
+  return (
+    <FlatList
+      data={mediaArray}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({item}) => <ListItem singleMedia={item} />}
+    />
+  );
+};
+
+export default List;
