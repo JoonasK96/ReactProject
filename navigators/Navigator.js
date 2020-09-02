2402f3060226f4b5d793ae95521f12123a1b56f0
joonavke <joonas.kelo@metropolia.com>
2020-09-02 10:43:29 +0300

    eka

---
+++ navigators/Navigator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Tab = createBottomTabNavigator();
+
+const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={Home} />
+        <Tab.Screen name='Profile' component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigator;
---
+++ views/Home.js
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+import {StatusBar} from 'expo-status-bar';
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import List from '../components/List';
+
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <List />
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
